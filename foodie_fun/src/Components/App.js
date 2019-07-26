53799ec3d5e0c3b5111ceb248e80c3b778495753
teej387 <teej387@yahoo.com>
2019-07-25 22:06:35 -0500

    Imported PrivateRoute into App.js

--- foodie_fun/src/Components/App.js
+++ foodie_fun/src/Components/App.js
@@ -3,12 +3,14 @@ import '../App.css';
 import { Route } from 'react-router-dom'
 import Register from './Register'
 import Dashboard from './Dashboard'
+import PrivateRoute from './PrivateRoute'
 
 class App extends React.Component {
 
   render() {  
     return (
       <div className="App">
+        <PrivateRoute exact path='/' component={Dashboard} />
         <Route exact path='/register' component={Register} />
       </div>
     );
