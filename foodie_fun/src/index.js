e1e86d0cb2e754642a5ed2119c0d41097e45be66
teej387 <teej387@yahoo.com>
2019-07-25 19:28:47 -0500

    Set up index.js for redux store and browserRouter

--- foodie_fun/src/index.js
+++ foodie_fun/src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducer from './reducer'
 
+const store = createStore(
+  reducer,
 
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+  )
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
