019fec68d94401fe5d3756fc94a226df56b1d9da
teej387 <teej387@yahoo.com>
2019-07-25 20:03:02 -0500

    Created login() action

--- foodie_fun/src/actions.js
+++ foodie_fun/src/actions.js
@@ -4,7 +4,13 @@ export const CREATE_USER_START = 'CREATE_USER_START'
 export const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS'
 export const CREATE_USER_FAILED = 'CREATE_USER_FAILED'
 
-export function createUser() {
+export const LOGIN_START = 'LOGIN_START'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILED = 'LOGIN_FAILED'
+
+
+
+export function createUser(username, password) {
   return (dispatch) => {
     dispatch({type: CREATE_USER_START})
 
@@ -16,6 +22,22 @@ export function createUser() {
       .catch((err) => {
         const payload = err.response ? err.response.data : err
         dispatch({type: CREATE_USER_FAILED, payload})
+      })
+  }
+}
+
+export function login(username, password) {
+  return (dispatch) => {
+    dispatch({type: LOGIN_START})
+
+    return axios.post('https://build-week-foodiefun.herokuapp.com/api/auth/login', {username, password})
+      .then((res) => {
+        localStorage.setItem('token', res.data.token)
+        dispatch({ type: LOGIN_SUCCESS })
+      })
+      .catch((err) => {
+        const payload = err.response ? err.response.data : err
+        dispatch({ type: LOGIN_FAILED, payload })
       })
   }
 }
