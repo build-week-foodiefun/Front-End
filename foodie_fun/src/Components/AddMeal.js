c6109794cf72b9f6fdf86aa00e7bddfd6a01d7b1
teej387 <teej387@yahoo.com>
2019-07-27 12:25:04 -0500

    Bug in addMeal()

--- foodie_fun/src/Components/AddMeal.js
+++ foodie_fun/src/Components/AddMeal.js
@@ -89,14 +89,15 @@ class AddMeal extends React.Component {
         <h2 className='addMealHeader'>Was it amazing? The worst ever? Add it so you'll have a way to remember!</h2>
         <input className='restName' type='text' name='restaurant_name' placeholder='Restaurant Name' value={restaurant_name} onChange={this.changeHandler} /><br />
         <input className='restType' type='text' name='restaurant_type' placeholder='Type of Restaurant' value={restaurant_type} onChange={this.changeHandler} /><br />
-        <label className='photoLabel' for='item_photo'>Choose a Restaurant Photo:</label>
+        <label className='photoLabel' htmlFor='item_photo'>Choose a Restaurant Photo:</label>
         <input className='itemPhoto' id='item_photo' type='file' name='item_photo' accept='image/*' value={item_photo} onChange={this.changeHandler} /><br />
         <input className='itemName' type='text' name='item_name' placeholder='The Name of the Meal' value={item_name} onChange={this.changeHandler} required /><br />
         <StarRatingComponent className='rating' name={'rating'} starCount={5} value={food_rating} onStarClick={this.onStarClick} emptyStarColor={'RGBA(255,205,80,0.5)'} renderStarIcon={() => <span role='img' aria-label='burger'><i className="fas fa-hamburger"></i></span>} /><br />
         <input className='comment' type='text' name='item_comment' placeholder='Leave a Comment' value={item_comment} onChange={this.changeHandler} /><br />
         <input className='waitTime' type='text' name='wait_time' placeholder='How long did you wait?' value={wait_time} onChange={this.changeHandler} /><br />
-        <label className='dateLabel' for='date_visited'>The Date You Ate:</label>
+        <label className='dateLabel' htmlFor='date_visited'>The Date You Ate:</label>
         <input className='visitDate' type='date' id='date_visited' name='date_visited' value={date_visited} onChange={this.changeHandler} /><br />
+        <button className='addButton' type='submit'>Add This Meal</button>
       </form>
     )
   }
