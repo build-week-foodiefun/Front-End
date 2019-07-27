de9d9e48570d55318477c3ab60e317d5314c541a
teej387 <teej387@yahoo.com>
2019-07-26 19:40:50 -0500

    Created AddMeal Component and added initial state.

--- foodie_fun/src/Components/AddMeal.js
+++ foodie_fun/src/Components/AddMeal.js
@@ -1,1 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addMeal } from '../actions'
+
+class AddMeal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      restaurant_name: '',
+      restaurant_type: '',
+      item_photo: '',
+      item_name: '',
+      food_rating: '',
+      item_comment: '',
+      wait_time: '',
+      date_visited: '',
+    }
+  }
+}
