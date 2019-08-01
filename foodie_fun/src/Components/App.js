27d9ce2d1d9e61943a3d52404ee19cbe9c16dcfd
teej387 <teej387@yahoo.com>
2019-08-01 00:00:38 -0500

    Added some styling for Home component.

--- foodie_fun/src/Components/App.js
+++ foodie_fun/src/Components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import '../Table.css'
 import { Route, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Register from './Register'
